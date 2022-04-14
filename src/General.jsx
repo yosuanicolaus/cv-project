1c63bf100ab7b7a008d701e577a577f76f9980fd
Yosua Nicolaus <cryodawn257@gmail.com>
2022-04-15 00:13:38 +0700

    create TextInput component & functionality

--- src/General.jsx
+++ src/General.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import TextInput from "./components/TextInput";
 
 class General extends React.Component {
   render() {
     return (
       <div>
         <h2>General</h2>
-        <div>John Doe</div>
+        <TextInput placeholder="input name here" id="name" />
+        <TextInput placeholder="write a short bio about yourself" id="bio" />
+        <TextInput placeholder="your email here" id="mail" type="email" />
+        <TextInput placeholder="your phone number here" id="phone" type="tel" />
       </div>
     );
   }
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+class TextInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.type = props.type;
+    this.id = props.id;
+    this.placeholder = props.placeholder;
+    this.state = {
+      value: "",
+    };
+  }
+
+  inputHandler = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  onSubmitInput = (e) => {
+    e.preventDefault();
+    console.log("submitted " + this.state.value);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitInput}>
+        <input
+          type={this.type}
+          name={this.id}
+          id={this.id}
+          placeholder={this.placeholder}
+          onChange={this.inputHandler}
+          value={this.state.value}
+        />
+      </form>
+    );
+  }
+}
+
+TextInput.defaultProps = {
+  placeholder: "placeholder...",
+  type: "text",
+};
+
+export default TextInput;
